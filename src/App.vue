6c464e7264a88674c2c52655db33bb8d575ac779
yefang <bella_yeh@163.com>
2022-09-22 16:59:57 +0800

    22.9.22-1 router change

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <router-link to="Home">首页</router-link>
-    <router-link to="About">关于</router-link>
-    <router-view/>
+    <router-link to="Home" tag="button">首页</router-link>
+    <router-link to="About" tag="button">关于</router-link>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.router-link-active {
+  color: #f00;
 }
 </style>
